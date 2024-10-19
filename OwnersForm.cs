58548c0507bfc5c72c74664b1090eda409b838ba
seroven <sebastianrodriguezventura@gmail.com>
2024-10-19 18:06:49 -0500

    docs: explanatory comments inline

--- OwnersForm.cs
+++ OwnersForm.cs
@@ -21,6 +21,7 @@ namespace T2_FP_01
             G2_Set_Items_To_View();
         }
 
+        // Setear los items en el DataGrid del formulario
         private void G2_Set_Items_To_View()
         {
             G2_Owners_DataGrid.Rows.Clear();
@@ -30,6 +31,7 @@ namespace T2_FP_01
             }
         }
 
+        // Limpiar el formulario
         private void G2_Clean_Txt()
         {
             G2_Name_Txt.Clear();
@@ -37,17 +39,21 @@ namespace T2_FP_01
             G2_Phone_Txt.Clear();
         }
 
+        // Valida que haya texto escrito en las cajas de texto. Sino, retorna una excepción con el nombre del campo vacío.
         private string G2_Validate_Txt(string fieldName, string text)
         {
             if (text.Length == 0) throw new Exception($"El campo {fieldName} está vacío");
             return text;
         }
 
+        // Evento de agregar una nueva mascota
         private void G2_Add_Btn_Click(object sender, EventArgs e)
         {
+            // Inicialización de variables
             string name, address, phone;
             try
             {
+                // Obtención de datos del formulario
                 name = G2_Validate_Txt("Nombre", G2_Name_Txt.Text);
                 address = G2_Validate_Txt("Dirección", G2_Address_Txt.Text);
                 phone = G2_Validate_Txt("Teléfono", G2_Phone_Txt.Text);
@@ -56,15 +62,25 @@ namespace T2_FP_01
                 MessageBox.Show(ex.Message);
                 return;
             }
-          
+
+            // Creación de la instancia dueño
             G2Owner owner = new G2Owner(name, address, phone);
+
+            // Agregando al arreglo
             G2OwnerArray.owners.Add(owner);
+
+            // Mostrando los dueños nuevamente en el DataGrid
             G2_Set_Items_To_View();
+
+            // Actualizando los items de la lista desplegable de dueños en el formulario de mascotas
             MenuForm.petsForm.G2_Set_Owners_In_Combobox();
+
+            // Limpiando el formulario
             G2_Clean_Txt();
             MessageBox.Show("Dueño registrado");
         }
 
+        // Evento para limpiar el formulario
         private void G2_Clean_Btn_Click(object sender, EventArgs e)
         {
             G2_Clean_Txt();
--- PetsForm.cs
+++ PetsForm.cs
@@ -23,6 +23,7 @@ namespace T2_FP_01
             G2_Set_Items_To_View();
         }
         
+        // Setear los dueños en el combobox del formulario
         public void G2_Set_Owners_In_Combobox()
         {
             G2_Owner_Cbx.Items.Clear();
@@ -32,6 +33,7 @@ namespace T2_FP_01
             }
         }
 
+        // Setear los items en el DataGrid del formulario
         private void G2_Set_Items_To_View()
         {
             G2_Pets_DataGrid.Rows.Clear();
@@ -41,6 +43,7 @@ namespace T2_FP_01
             }
         }
 
+        // Limpiar el formulario
         private void G2_Clean_Txt()
         {
             G2_Name_Txt.Clear();
@@ -49,24 +52,29 @@ namespace T2_FP_01
             G2_Owner_Cbx.SelectedIndex = -1;
         }
 
+        // Valida que haya texto escrito en las cajas de texto. Sino, retorna una excepción con el nombre del campo vacío.
         private string G2_Validate_Txt(string fieldName, string text)
         {
             if (text.Length == 0) throw new Exception($"El campo {fieldName} está vacío");
             return text;
         }
 
+        // Valida que haya una opción seleccionada en las listas desplegables. Sino, retorna una excepción con el nombre del campo vacío.
         private int G2_Validate_Cbx(string fieldName, int index)
         {
             if (index < 0) throw new Exception($"No ha seleccionado un elemento en la lista desplegable de {fieldName} ");
             return index;
         }
 
+        // Evento de agregar una nueva mascota
         private void G2_Add_Btn_Click(object sender, EventArgs e)
         {
+            // Inicialización de variables
             string name, breed, age;
             G2Owner owner;
             try
             {
+                // Obtención de datos del formulario
                 name = G2_Validate_Txt("Nombre", G2_Name_Txt.Text);
                 breed = G2_Validate_Txt("Raza", G2_Breed_Txt.Text);
                 age = G2_Validate_Txt("Edad", G2_Age_Txt.Text);
@@ -78,24 +86,34 @@ namespace T2_FP_01
                 return;
             }
             
-
+            // Creación de la instancia mascota
             G2Pet pet = new G2Pet(name, age, breed, owner);
+
+            // Agregando al arreglo
             G2PetArray.pets.Add(pet);
+
+            // Mostrando las mascotas nuevamente en el DataGrid
             G2_Set_Items_To_View();
+
+            // Limpiando el formulario
             G2_Clean_Txt();
             MessageBox.Show("Mascota registrada");
         }
 
+        // Evento para limpiar el formulario
         private void G2_Clean_Btn_Click(object sender, EventArgs e)
         {
             G2_Clean_Txt();
         }
 
+        // Evento para buscar una mascota
         private void G2_Search_By_Name_Btn_Click(object sender, EventArgs e)
         {
+            // Inicialización de variables
             string name;
             try
             {
+                // Obtención de datos del formulario
                 name = G2_Validate_Txt("Nombre", G2_Search_By_Name_Txt.Text);
             }
             catch (Exception ex)
@@ -104,9 +122,10 @@ namespace T2_FP_01
                 return;
             }
 
-
+            // Búsqueda de la mascota recorriendo el arreglo
             foreach (G2Pet pet in G2PetArray.pets)
             {
+                // Muestra de la mascota al hallar una coincidencia
                 if (pet.G2_name.ToLower() == name.ToLower())
                 {
                     MessageBox.Show($"Mascota encontrada!\nNombre: {pet.G2_name}\nRaza: {pet.G2_breed}\nEdad: {pet.G2_age}\nDueño: {pet.G2_owner.G2_name}");
